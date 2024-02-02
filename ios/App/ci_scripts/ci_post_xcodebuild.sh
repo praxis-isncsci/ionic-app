6842eb2047a58fba025febba6f5bab6106c52d05
eddiemachete <elguardianenelsendero@hotmail.com>
2024-02-02 03:12:59 +0000

    Updated post build commands

--- ios/App/ci_scripts/ci_post_xcodebuild.sh
+++ ios/App/ci_scripts/ci_post_xcodebuild.sh
@@ -1,10 +1,9 @@
- #!/bin/zsh
+#!/bin/zsh
 
- echo "🧩 Stage: POST-Xcode Build is activated .... "
+echo "🧩 Stage: POST-Xcode Build is activated .... "
 
- # You can add additional scripts here...
+# You can add additional scripts here...
 
- echo "🎯 Stage: POST-Xcode Build is DONE .... "
+echo "🎯 Stage: POST-Xcode Build is DONE .... "
 
- exit 0
- 
+exit 0

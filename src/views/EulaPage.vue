75d2512b352bcbc11aec11d3dbb3310422ce350b
Anna Mironyuk <annamironyuk@Annas-Mac-mini.local>
2024-08-01 13:09:41 -0700

    accepted eula page

--- src/views/EulaPage.vue
+++ src/views/EulaPage.vue
@@ -2,12 +2,18 @@
   <MainLayout title="EULA">
     <div class="ion-padding">
       <div v-html="eulaContent"></div>
-      <ion-button @click="acceptEula">Accept</ion-button>
+      <div v-if="!isEulaAccepted">
+        <ion-button @click="acceptEula">Accept</ion-button>
+      </div>
+      <div v-else>
+        <p>Accepted</p>
+      </div>
     </div>
   </MainLayout>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import {
   IonButton
 } from '@ionic/vue'
@@ -16,8 +22,23 @@ import MainLayout from './MainLayout.vue';
 import { eulaContent } from '../eulaContent.ts'
 
 const router = useRouter();
+const isEulaAccepted = ref(false);
+
+const checkEulaAccepted = (): void => {
+  const eulaState = localStorage.getItem('eulaAccepted');
+  if (eulaState === 'true') {
+    isEulaAccepted.value = true;
+  } else {
+    isEulaAccepted.value = false;
+  }
+};
+
 const acceptEula = (): void => {
   localStorage.setItem('eulaAccepted', 'true');
   router.push('/home');
 };
+
+onMounted(() => {
+  checkEulaAccepted();
+});
 </script>

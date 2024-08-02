003b243449c7119a02ef81d42266a2bf94c00186
Anna Mironyuk <annamironyuk@Annas-Mac-mini.local>
2024-08-02 12:57:35 -0700

    temp styling

--- src/views/EulaPage.vue
+++ src/views/EulaPage.vue
@@ -1,13 +1,13 @@
 <template>
   <MainLayout title="EULA">
     <div class="ion-padding">
-      <div v-html="eulaContent"></div>
+      <div v-html="eulaContent" class="eula-content"></div>
       <div v-if="!isEulaAccepted">
         <ion-button @click="acceptEula">Accept</ion-button>
       </div>
-      <div v-else>
-        <p>
-          <ion-icon :icon="checkmarkOutline"></ion-icon>
+      <div v-else class="accepted-container">
+        <p class="accepted-text">
+          <ion-icon :icon="checkmarkOutline" class="checkmark-icon"></ion-icon>
           Accepted
         </p>
       </div>
@@ -51,3 +51,36 @@ watch(route, () => {
   checkEulaAccepted();
 });
 </script>
+
+<style scoped>
+.eula-content {
+  display: flex;
+  flex-direction: column;
+  font-family: var(--ion-font-family, 'Inter', Arial, sans-serif);
+  font-size: var(--ion-font-size, 14px);
+  font-weight: 400;
+  line-height: 20px;
+}
+
+.accepted-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.accepted-text {
+  display: flex;
+  align-items: center;
+  font-size: var(--ion-font-size, 16px);
+  font-weight: 600;
+  color: var(--ion-color-success);
+}
+
+.checkmark-icon {
+  margin-right: 5px;
+  color: var(--ion-color-success);
+  border: 1px solid var(--ion-color-success);
+  border-radius: 2px;
+}
+</style>

ce0f4508ef8e358c2281d085db20b17d75739fe5
Anna Mironyuk <annamironyuk@Annas-Mac-mini.local>
2024-11-26 14:28:21 -0800

    updating savedwoksheets when changed

--- src/views/SavedWorksheetsPage.vue
+++ src/views/SavedWorksheetsPage.vue
@@ -56,7 +56,7 @@ onMounted(async () => {
     // const storedData = JSON.parse(localStorage.getItem(`${APP_PREFIX}meta`) || '[]');
     // savedWorksheets.value = storedData;
     await worksheets.loadMeta();
-    savedWorksheets.value = worksheets.getAllMeta();
+    savedWorksheets.value = [...worksheets.getAllMeta()];
 });
 
 const close_onClick = () => {

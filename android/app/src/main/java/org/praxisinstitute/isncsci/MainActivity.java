d0fd61cfa1b557a8d86ec301c3588eb2d12330fd
Anna Mironyuk <annamironyuk@Annas-Mac-mini.local>
2025-01-22 13:17:25 -0800

    font size android fix

--- android/app/src/main/java/org/praxisinstitute/isncsci/MainActivity.java
+++ android/app/src/main/java/org/praxisinstitute/isncsci/MainActivity.java
@@ -1,5 +1,20 @@
 package org.praxisinstitute.isncsci;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        WebView webView = (WebView) this.bridge.getWebView();
+        WebSettings webSettings = webView.getSettings();
+
+        // force textZoom to 100% (font to be const size - accessibiluty is off)
+        webSettings.setTextZoom(100);
+    }
+}
